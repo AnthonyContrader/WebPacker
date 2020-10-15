a9704116dfd79bfd6111edf32f784f11803a5935
silviamiati <silvia.miati@gmail.com>
2020-10-15 16:36:45 +0200

    Read View

--- src/main/java/it/contrader/converter/ProjectConverter.java
+++ src/main/java/it/contrader/converter/ProjectConverter.java
@@ -3,9 +3,9 @@ package it.contrader.converter;
 import java.util.ArrayList;
 import java.util.List;
 
-<<<<<<< HEAD
-import it.contrader.dto.UserDTO;
-import it.contrader.model.User;
+
+import it.contrader.dto.ProjectDTO;
+import it.contrader.model.Project;
 
 
 public class ProjectConverter   {
@@ -16,43 +16,14 @@ public class ProjectConverter   {
 	 */
 	public ProjectDTO toDTO(Project project) {
 		ProjectDTO projectDTO = new ProjectDTO(project.getProjectId(), project.getUserId(), project.getProjectName());
-=======
-import it.contrader.dto.ProjectDTO;
-import it.contrader.model.Project;
-
-/**
- * 
- * @author Raffaele
- *
- */
-public class ProjectConverter   {
-	
-	/**
-	 * Crea un oggetto di tipo ProjectDTO e lo riempie con i campi del parametro project di tipo Project.
-	 * Notare l'uso del metodo get() per ottenere il valore dell'attributo-
-	 */
-	public ProjectDTO toDTO(Project project) {
-		ProjectDTO projectDTO = new ProjectDTO(project.getProjectid(), project.getUserid(),project.getProjectname());
->>>>>>> bdc958bcaa7c914cf869fd5ec1c7842f7e00bb39
 		return projectDTO;
 	}
 
-	/**
-<<<<<<< HEAD
-	 * Crea un oggetto di tipo Project e lo riempie con i campi del parametro user di tipo ProjectDTO.
-	 * Notare l'uso del metodo get() per ottenere il valore dell'attributo-
-	 */
+
+	
 	public Project toEntity(ProjectDTO projectDTO) {
-		Project projectDTO = new User(projectDTO.getProjectId(), projectDTO.getUserId(), projectDTO.getProjectName());
-		return projectDTO;
-=======
-	 * Crea un oggetto di tipo Project e lo riempie con i campi del parametro project di tipo ProjectDTO.
-	 * Notare l'uso del metodo get() per ottenere il valore dell'attributo-
-	 */
-	public Project toEntity(ProjectDTO projectDTO) {
-		Project project = new Project(project.getProjectid(), project.getUserid(),project.getProjectname());
+		Project project = new Project(projectDTO.getProjectid(), projectDTO.getUserid(),projectDTO.getProjectname());
 		return project;
->>>>>>> bdc958bcaa7c914cf869fd5ec1c7842f7e00bb39
 	}
 	
 	/**
@@ -70,11 +41,5 @@ public class ProjectConverter   {
 		}
 		return projectDTOList;
 	}
-
 	
-	
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> bdc958bcaa7c914cf869fd5ec1c7842f7e00bb39
--- src/main/java/it/contrader/view/project/ProjectReadView.java
+++ src/main/java/it/contrader/view/project/ProjectReadView.java
@@ -1,5 +1,55 @@
 package it.contrader.view.project;
 
-public class ProjectReadView {
+import it.contrader.controller.Request;
+
+import it.contrader.dto.ProjectDTO;
+import it.contrader.main.MainDispatcher;
+import it.contrader.view.AbstractView;
+
+public class ProjectReadView extends AbstractView{
+	private int id;
+	private Request request;
+	private final String mode = "READ";
+
+	public ProjectReadView() {
+	}
+
+	/**
+	 * Se la request è null (ovvero quando arriva dal controller con mode GETCHOICE e choice L 
+	 * il metodo è vuoto.
+	 * 
+	 * Altrimenti se arriva con uno user nella request (ovvero quando arriva
+	 * dal controller con mode READ) mostra lo user. In questo caso torna alla UserView senza eseguire
+	 * gli altri due metodi
+	 */
+	@Override
+	public void showResults(Request request) {
+		if (request != null) {
+			ProjectDTO project = (ProjectDTO) request.get("project");
+			System.out.println(project);
+			MainDispatcher.getInstance().callView("Project", null);
+		}
+	}
+
+	
+	/**
+	 * chiede all'utente di inserire l'id dell'utente da leggere
+	 */
+	@Override
+	public void showOptions() {
+		System.out.println("Inserisci l'ID del progetto:");
+		id = Integer.parseInt(getInput());
+	}
+
+	/**
+	 * impacchetta una request con l'id del progetto da leggere e la manda al controller tramite il Dispatcher
+	 */
+	@Override
+	public void submit() {
+		request = new Request();
+		request.put("id", id);
+		request.put("mode", mode);
+		MainDispatcher.getInstance().callAction("Project", "doControl", request);
+	}
 
 }
