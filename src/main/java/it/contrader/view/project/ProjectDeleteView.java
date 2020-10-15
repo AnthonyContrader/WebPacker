2a65ed25c28c5195b84a209a04b85bacf4405edf
Davidex95 <davidex0495@gmail.com>
2020-10-15 16:35:55 +0200

    Aggiunta Project Delet View

--- src/main/java/it/contrader/view/project/ProjectDeleteView.java
+++ src/main/java/it/contrader/view/project/ProjectDeleteView.java
@@ -1,5 +1,49 @@
-package it.contrader.view.project;
+package it.contrader.view.user;
 
-public class ProjectDeleteView {
+import it.contrader.controller.Request;
+import it.contrader.main.MainDispatcher;
+import it.contrader.view.AbstractView;
+
+public class UserDeleteView extends AbstractView {
+	private Request request;
+    private int projectid;
+	private final String mode = "DELETE";
+
+	public UserDeleteView() {
+	}
+
+	/**
+	 * Se la request non è nulla (ovvero se si arriva dalla mode DELETE del controller) mostra
+	 * l'esito dell'operazione
+	 */
+	@Override
+	public void showResults(Request request) {
+		if (request!=null) {
+			System.out.println("Cancellazione andata a buon fine.\n");
+			MainDispatcher.getInstance().callView("User", null);
+		}
+	}
+
+	/**
+	 * Chiede all'utente di inserire il projectid dell'utente da cancellare
+	 */
+	@Override
+	public void showOptions() {
+			System.out.println("Inserisci projectid dell'utente:");
+			projectid = Integer.parseInt(getInput());
+
+	}
+
+	/**
+	 * impacchetta la request con il projectid dell'utente da cancellare
+	 */
+	@Override
+	public void submit() {
+		request = new Request();
+		request.put("projectid", projectid);
+		request.put("mode", mode);
+		MainDispatcher.getInstance().callAction("User", "doControl", request);
+	}
+
 
 }
