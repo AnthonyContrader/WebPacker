72ea6f7590660890cf7003e6ba05324033dd6657
xMarcoMax <stefano.marcomini94@gmail.com>
2020-10-27 10:27:42 +0100

    Modifica pom.xml

--- src/main/java/it/contrader/SpringWebApplication.java
+++ src/main/java/it/contrader/SpringWebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 public class SpringWebApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringWebApplication.class, args);
 	}
 }
