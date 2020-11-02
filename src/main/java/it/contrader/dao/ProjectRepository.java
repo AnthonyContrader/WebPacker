4d5dd6602c9264b90352dff3431489d5c40ddab6
Davidex95 <davidex0495@gmail.com>
2020-11-02 17:33:35 +0100

    ProjectRepository

---
+++ src/main/java/it/contrader/dao/ProjectRepository.java
@@ -0,0 +1,26 @@
+package it.contrader.dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import it.contrader.model.Project;
+
+
+/**
+ * Estende CrudRepository ed eredita tutti i metodi di CRUD. 
+ * Definisce il metodo di login.
+ * 
+ * @author Vittorio Valent & Girolamo Murdaca
+ *
+ * @see CrudRepository
+ *
+ */
+@Repository
+@Transactional
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	Project findByProjectidAndUserid(String projectid, String userid);
+	
+}
