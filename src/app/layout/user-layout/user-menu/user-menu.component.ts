487b219b47b555fb969ad830eccb182373729dc4
silviamiati <silvia.miati@gmail.com>
2020-11-03 10:40:04 +0100

    User-Layout

--- src/app/layout/user-layout/user-menu/user-menu.component.ts
+++ src/app/layout/user-layout/user-menu/user-menu.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserMenuComponent implements OnInit {
 
-  constructor() { }
+  isUserCollapsed = false;
+  isClientCollapsed = false;
+  isAccountCollapsed = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  logout() {
+    localStorage.clear();
+    this.router.navigateByUrl('');
+  }
+
+  userscollapse() {
+    if (this.isUserCollapsed === false) {
+      this.isUserCollapsed = true;
+    } else { this.isUserCollapsed = false; }
+  }
+
+  accountcollapse() {
+    if (this.isAccountCollapsed === false) {
+      this.isAccountCollapsed = true;
+    } else { this.isAccountCollapsed = false; }
+  }
 }
